d42a79a1533caab7370a61c1a1c00af43a84facd
Manoj Sahoo <manoj.sahoo2@ey.com>
2024-09-06 21:58:51 +0530

    Javascript assignments

---
+++ JavaScript-Practise/Basic/script.js
@@ -0,0 +1,22 @@
+console.log("Hello World JS!")
+
+console.log(23 + 97)
+
+// Define 6 different numbers
+let num1 = 1;
+let num2 = 2;
+let num3 = 3;
+let num4 = 4;
+let num5 = 5;
+let num6 = 6;
+
+// Sum the numbers
+let sum = num1 + num2 + num3 + num4 + num5 + num6;
+
+// Log the result to the console
+console.log(sum);
+
+console.log((4 + 6 + 9) / 77);
+
+let a = 10
+console.log(a)
---
+++ JavaScript-Practise/Rock-Paper-Scissor/script.js
@@ -0,0 +1,9 @@
+function getComputerChoice(){
+    let randomNum = Math.floor(Math.random() * 3);
+    switch(randomNum){
+        case 0 : return "Rock";
+        case 1 : return "Paper";
+        case 2 : return "Scissor";
+    }
+}
+console.log(getComputerChoice())
